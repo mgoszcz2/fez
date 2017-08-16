7fda41e79b5279d1396277bb2b74c71be1097be0
Maciej Goszczycki <mgoszcz2@gmail.com>
2017-08-16 22:01:53 +0200

    Initial Commit

---
+++ Fez/Fez.h
@@ -0,0 +1,19 @@
+//
+//  Fez.h
+//  Fez
+//
+//  Created by Maciej Goszczycki on 16/08/2017.
+//  Copyright © 2017 Maciej. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Fez.
+FOUNDATION_EXPORT double FezVersionNumber;
+
+//! Project version string for Fez.
+FOUNDATION_EXPORT const unsigned char FezVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fez/PublicHeader.h>
+
+
